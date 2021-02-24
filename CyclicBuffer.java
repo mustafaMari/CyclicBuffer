dfde45e53e1881c15c4e3bc2bb07bc15d083eb5e
Mustafa Alhamoud <61988029+mustafaMari@users.noreply.github.com>
2021-02-24 12:26:05 +1200

    all classes were added

---
+++ CyclicBuffer.java
@@ -0,0 +1,39 @@
+package Class3task2;
+
+public class CyclicBuffer<E> {
+	private E [] array ; 
+	private int size=0;  
+	private int currentsize=0; 
+	private int get = 0 ; 
+	@SuppressWarnings("unchecked")
+	public CyclicBuffer(int size) { 
+		this.size=size; 
+		array = (E[]) new Object [size];
+		}
+	public void add(E ele) { 
+		if (isFull()) return; 
+			array[currentsize]= ele; 
+			currentsize++;
+ 	}
+	public boolean isEmpty() { 
+		if(get>=size) return true; 
+		return false; 
+	}
+	public boolean isFull() {
+		if (currentsize==size) return true; 
+		return false; 
+	}
+	public E get() { 
+		if(isEmpty()) return null;   
+		E currele = array[get]; 
+		get++; 
+		return currele; 
+	}
+	public String toString() {
+		String s="";
+		for (E x : array) {
+			s+=" "+x;
+		}
+		return s; 
+	}
+}
---
+++ Main.java
@@ -0,0 +1,14 @@
+package Class3task2;
+
+public class Task2{
+	public static void main(String[] args) { 
+		CyclicBuffer<Character> test= new CyclicBuffer<>(35);
+		String s="2 fihsuhf 43t3 3r3th"; 
+		for (char x :  s.toCharArray()) {
+			test.add(x);
+		}
+		while (!test.isEmpty()) {
+			System.out.println(test.get());
+		}
+	}
+}
